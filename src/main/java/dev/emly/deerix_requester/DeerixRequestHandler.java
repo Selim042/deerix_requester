f425c87db872dd9f38ad235cb17d454282131d92
Selim042 <042Selim@gmail.com>
2025-08-01 10:55:30 -0400

    Add Discord webhooks

--- src/main/java/dev/emly/deerix_requester/DeerixRequestHandler.java
+++ src/main/java/dev/emly/deerix_requester/DeerixRequestHandler.java
@@ -1,20 +1,32 @@
 package dev.emly.deerix_requester;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import api.deezer.DeezerApi;
+import api.deezer.exceptions.DeezerException;
+import api.deezer.objects.Album;
+import api.deezer.objects.Artist;
+import api.deezer.objects.Track;
 import com.moandjiezana.toml.Toml;
 import com.moandjiezana.toml.TomlWriter;
 
 public class DeerixRequestHandler {
 
+	final DeezerApi DEEZER_API;
+
 	public static final DeerixRequestHandler REQUEST = new DeerixRequestHandler();
 	public static final File DEERIX_CONFIG_PATH = new File("/config/deerix_settings.toml");
 	
 	private static final TomlWriter WRITER = new TomlWriter();
+
+	public DeerixRequestHandler() {
+		DEEZER_API = new DeezerApi();
+	}
 
 	private String getConfigContents() {
 		StringBuilder builder = new StringBuilder();
@@ -34,6 +46,24 @@ public class DeerixRequestHandler {
 		Toml toml = new Toml().read(getConfigContents());
 		return toml.toMap();
 	}
+
+	public boolean sendNotification(String art, String description) {
+		DiscordWebhook webhook = new DiscordWebhook(System.getenv("DISCORD_WEBHOOK"));
+		webhook.addEmbed(new DiscordWebhook.EmbedObject()
+				.setTitle("New Music Requested")
+				.setDescription(description)
+				.setColor(Color.GREEN)
+				// TODO: Add requested when we have auth
+				// .addField("Requested By", "Emily", true)
+				.setThumbnail(art)
+				.setImage("https://kryptongta.com/images/kryptontitle2.png"));
+        try {
+            webhook.execute(); //Handle exception
+			return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 	
 	public boolean requestArtist(int id, String note) {
 		Map<String, Object> tomlMap = getTomlFile();
@@ -47,12 +77,13 @@ public class DeerixRequestHandler {
 
 		try {
 			WRITER.write(tomlMap, DEERIX_CONFIG_PATH);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return true;
+			Artist artist = DEEZER_API.artist().getById(id).execute();
+			sendNotification(artist.getPicture(), "Artist: " + artist.getName());
+		} catch (IOException | DeezerException e) {
+            return false;
+        }
+
+        return true;
 	}
 
 	public boolean requestAlbum(int id, String note) {
@@ -67,9 +98,10 @@ public class DeerixRequestHandler {
 
 		try {
 			WRITER.write(tomlMap, DEERIX_CONFIG_PATH);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Album album = DEEZER_API.album().getById(id).execute();
+			sendNotification(album.getCover(), "Album: " + album.getTitle());
+		} catch (IOException | DeezerException e) {
+			return false;
 		}
 		
 		return true;
@@ -87,9 +119,10 @@ public class DeerixRequestHandler {
 
 		try {
 			WRITER.write(tomlMap, DEERIX_CONFIG_PATH);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Track track = DEEZER_API.track().getById(id).execute();
+			sendNotification(track.getAlbum().getCover(), "Track: " + track.getTitle());
+		} catch (IOException | DeezerException e) {
+			return false;
 		}
 		
 		return true;
